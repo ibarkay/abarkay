c4214341294c693bbe6e1540b604032d12f5f168
ibarkay <ibarkay@gmail.com>
2021-06-10 08:55:19 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+import { AppBar, Toolbar } from "@material-ui/core";
+
+// --------CSS---------
+import "./Css/App.css";
+// -------statics---------
+import backIMG from "./img/IMG_5640.jpg";
+import Logo from "./img/logos/l.png";
+// ------font-theming------
+const theme = createMuiTheme({
+	typography: {
+		fontFamily: ["Rubik", "cursive"].join(","),
+	},
+});
+
+// ------------------------
+
+export default function App() {
+	return (
+		<React.Fragment>
+			<MuiThemeProvider theme={theme}>
+				<div className="background">
+					<AppBar position="sticky" style={{ background: "none" }}>
+						<Toolbar edge="start">
+							<img src={Logo} alt="logo" style={{ height: 40 }} color="white" />
+						</Toolbar>
+					</AppBar>
+					<CssBaseline />
+
+					<Container maxWidth="l">
+						<Typography variant="h1" align="center">
+							אשדות ברקאי טיולי כרכרות סוסים
+						</Typography>
+						<Typography variant="h1" align="center">
+							english text
+						</Typography>
+					</Container>
+				</div>
+			</MuiThemeProvider>
+		</React.Fragment>
+	);
 }
-
-export default App;
